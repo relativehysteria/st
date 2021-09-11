e67f66352dd008f14b45efadca01bd7cf968d6dd
relativehysteria <rruziro@gmail.com>
2021-09-11 14:52:07 +0200

    Newterm

--- config.def.h
+++ config.def.h
@@ -94,9 +94,7 @@ char *termname = "st-256color";
 unsigned int tabspaces = 8;
 
 /* bg opacity */
-float alpha = 0.85;
-float alphaUnfocused = 0.75;
-
+float alpha = 0.85; float alphaUnfocused = 0.75;
 /* Terminal colors (16 first used in escape sequence) */
 static const char *colorname[] = {
 	/* 8 normal colors */
@@ -207,6 +205,7 @@ static Shortcut shortcuts[] = {
 	{ TERMMOD,              XK_J,           kscrolldown,    {.i =  1} },
 	{ ShiftMask,            XK_Insert,      selpaste,       {.i =  0} },
 	{ TERMMOD,              XK_Num_Lock,    numlock,        {.i =  0} },
+	{ TERMMOD,              XK_Return,      newterm,        {.i =  0} },
 };
 
 /*
