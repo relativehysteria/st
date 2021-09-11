199b9030c8c99d5e1515b9555df43c4a26f19b3a
relativehysteria <rruziro@gmail.com>
2021-09-11 14:20:54 +0200

    Fixed the 'Unfocussed' typo

--- config.def.h
+++ config.def.h
@@ -95,7 +95,7 @@ unsigned int tabspaces = 8;
 
 /* bg opacity */
 float alpha = 0.85;
-float alphaUnfocussed = 0.75;
+float alphaUnfocused = 0.75;
 
 /* Terminal colors (16 first used in escape sequence) */
 static const char *colorname[] = {
--- st.h
+++ st.h
@@ -124,4 +124,4 @@ extern unsigned int tabspaces;
 extern unsigned int defaultfg;
 extern unsigned int defaultbg;
 extern float alpha;
-extern float alphaUnfocussed;
+extern float alphaUnfocused;
