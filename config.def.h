233ec564ede2b60ba627fda21917446c95f47412
relativehysteria <rruziro@gmail.com>
2021-09-11 14:19:49 +0200

    Alpha + Focus

--- config.def.h
+++ config.def.h
@@ -93,6 +93,10 @@ char *termname = "st-256color";
  */
 unsigned int tabspaces = 8;
 
+/* bg opacity */
+float alpha = 0.85;
+float alphaUnfocussed = 0.75;
+
 /* Terminal colors (16 first used in escape sequence) */
 static const char *colorname[] = {
 	/* 8 normal colors */
